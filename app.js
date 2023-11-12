6c467c3b223dbfe6081d447f5fcd0f55324cdb2e
JAINAM576 <het170246@gmail.com>
2023-11-12 18:43:31 +0530

    doneport

--- app.js
+++ app.js
@@ -416,7 +416,9 @@ app.post("/spipa/addDepartment", (req, res) => {
   res.status(200).json(1);
 });
 
-app.listen(3000, () => {
+const PORT=process.env.PORT|| 3000;
+
+app.listen(PORT, () => {
   console.log("Server started on port 3000");
 });
 app.get("/reject/:id", (req, res) => {
